9f5c7116d04f4a57ee778e4a58bf4018353e170f
STLN-S1NcLair <mashumaro2584@gmail.com>
2024-08-18 13:40:49 +0900

    Initial commit

---
+++ src/main/java/net/stln/launchersandarrows/LaunchersAndArrows.java
@@ -0,0 +1,16 @@
+package net.stln.launchersandarrows;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LaunchersAndArrows implements ModInitializer {
+	public static final String MOD_ID = "launchers_and_arrows";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/stln/launchersandarrows/LaunhchersAndArrowsClient.java
@@ -0,0 +1,10 @@
+package net.stln.launchersandarrows;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LaunhchersAndArrowsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
